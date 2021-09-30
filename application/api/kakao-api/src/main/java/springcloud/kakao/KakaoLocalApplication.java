9aa601bcb27d57cd0889c0daa7cb34b5ac18ad3b
choi-ys <project.log.062@gmail.com>
2021-09-30 20:20:28 +0900

    [2021-09-30]용석:application:api:kakao-api 모듈 구동 설정 추가

--- application/api/kakao-api/src/main/java/springcloud/kakao/KakaoLocalApplication.java
+++ application/api/kakao-api/src/main/java/springcloud/kakao/KakaoLocalApplication.java
@@ -2,11 +2,13 @@ package springcloud.kakao;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author : choi-ys
  * @date : 2021-09-30 오후 3:50
  */
+@EnableEurekaClient
 @SpringBootApplication(scanBasePackages = "springcloud")
 public class KakaoLocalApplication {
 
