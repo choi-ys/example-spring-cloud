921b4197e8a44e5650a788ed99cbbf3f805c5637
choi-ys <project.log.062@gmail.com>
2021-09-30 20:21:43 +0900

    [2021-09-30]용석:application:api:hello-api Eureka client 등록 설정 및 application 구동 설정 추가

--- application/api/hello-api/src/main/java/springcloud/hello/HelloApplication.java
+++ application/api/hello-api/src/main/java/springcloud/hello/HelloApplication.java
@@ -2,12 +2,14 @@ package springcloud.hello;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @author : choi-ys
  * @date : 2021-09-29 오후 5:26
  */
-@SpringBootApplication
+@EnableDiscoveryClient
+@SpringBootApplication(scanBasePackages = "springcloud.hello")
 public class HelloApplication {
 
     public static void main(String[] args) {
