ee3d498f7027f4b1ca80dc9b10fa147eb01014c4
choi-ys <project.log.062@gmail.com>
2021-10-01 22:33:32 +0900

    [2021-10-01]용석:gateway 모듈 계층 구조 변경
     - AS-IS : application > gateway
     - TO-BE : application > api > gateway

--- application/api/gateway/src/main/java/springcloud/gateway/GatewayApplication.java
+++ application/api/gateway/src/main/java/springcloud/gateway/GatewayApplication.java
@@ -3,7 +3,6 @@ package springcloud.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author : choi-ys
